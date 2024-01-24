6a705885a3182805285cb965c08c3fd76a8b20f1
Asadbek <ibnnumon@gmail.com>
2024-01-24 17:59:32 +0500

    finish

---
+++ src/api/endpoints.js
@@ -0,0 +1,31 @@
+export const endpoints = {
+  base: "/api",
+  user:"/user",
+  login:"login",
+  signIn: "/signin",
+  signUp:"/signup",
+  otp:"/otp",
+
+
+  become:"/become-tutor",
+  
+
+  google:"/user-auth-google",
+  signUpGoogle:"/callback",
+  signInGoogle:"/google",
+
+
+  facebook:"user-auth-facebook",
+  signUpFacebook:"/callback",
+  signInFacebook:"/facebook",
+  
+  resetPsw: "/reset-password",
+  changePsw: "/change-password",
+  resetVerify: "/resetverify",
+
+
+
+  courses: "/courses",
+
+  file: "/file",
+};
